ca398d8261628c9d24296bcea797f6057cf95e48
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2025-04-07 15:40:57 -0400

    add comments to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// @Component is a decorator that tells Angular to create a new component
+// selector is the CSS selector that identifies this component in the HTML
+// templateUrl is the HTML file that describes the UI of the component
+// styleUrl is the CSS file that describes the styles of the component  
+// export class AppComponent is the class that defines the component
+// title is a property that defines the title of the component
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
