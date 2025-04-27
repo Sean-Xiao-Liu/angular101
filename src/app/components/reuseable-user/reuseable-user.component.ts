1a6a98ab021e7b3c6a5207f13ae2182a77493556
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2025-04-27 18:03:57 -0400

    Signal are for newer project while the input decorator is more common for old Angular projects

--- src/app/components/reuseable-user/reuseable-user.component.ts
+++ src/app/components/reuseable-user/reuseable-user.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-reuseable-user',
@@ -11,14 +11,26 @@ export class ReuseableUserComponent {
   // need to use the ! operator to tell the compiler that the property is not null or undefined
   // these input are coming from the app.component.html file
   // required: true means that the property is required and must be provided when the component is used
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
-  @Input({required: true}) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // id = input.required<string>();
+
+  // computed property
+  // imagePath = computed(() => {
+  //   return 'assets/images/users/' + this.avatar();
+  // });
 
   onSelectUser() {
-   console.log('Selected user:', this.name,'with id:', this.id);
+    // the () is used to call the function when property are read as single value
+    // However if use @Input decorator, the property is read as single value, and the () is not needed
+    console.log('Selected user:', this.name, 'with id:', this.id);
   }
 
+  // This is the way to define a property when using @Input decorator
   get imagePath() {
     return 'assets/images/users/' + this.avatar;
   }
