c2408211e34ade3940d8457b19874e67f8923438
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2025-05-04 15:00:16 -0400

    output user specific tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { DUMMY_TASKS } from '../../../assets/tasks/dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -13,4 +14,10 @@ export class TasksComponent {
   // the name property is passed from the app.component.html file
   // and the name property is used in the tasks.component.html file
   @Input({ required: true }) name!: string;
+  @Input({ required: true }) userId!: string;
+  tasks = DUMMY_TASKS;
+
+  get filteredTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
