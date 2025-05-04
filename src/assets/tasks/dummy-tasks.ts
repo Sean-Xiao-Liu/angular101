31e82f7d7e5bf1a617a26ba29343b67d8df355de
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2025-05-04 14:27:33 -0400

    add dummy tasks

---
+++ src/assets/tasks/dummy-tasks.ts
@@ -0,0 +1,16 @@
+export const DUMMY_TASKS = [
+    {
+        id: 't1',
+        title: 'Task 1',
+        description: 'Task 1 description',
+        completed: false,
+        createdAt: '2025-01-01',
+    },
+    {
+        id: 't2',
+        title: 'Task 2',
+        description: 'Task 2 description',
+        completed: false,
+        createdAt: '2025-01-01',
+    }
+]
